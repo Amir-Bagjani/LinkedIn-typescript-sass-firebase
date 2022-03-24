70d9f7b9fb55ba17a7610d6417344cef815254b9
amirbagjni <amirbagjani@gmail.com>
2022-03-25 04:05:38 +0430

    login css style fixed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,13 +5,15 @@ const Login = () => {
   return (
     <div className="login-page">
         <div className="wrapper">
+        
             <header>
                 <img src="images/login-logo.svg" alt="linkedin" />
                 <nav className="navbar">
-                    <a href="#" className="join-btn">Join now</a>
-                    <a href="#" className="signin-btn">Sign in</a>
+                    <a className="join-btn">Join now</a>
+                    <a className="signin-btn">Sign in</a>
                 </nav>
             </header>
+
 
             <section>
                 <div className="hero">
@@ -23,7 +25,8 @@ const Login = () => {
                 </div>
                 <img src="images/login-hero-2.svg" alt="login-hero-image" />
             </section>
-        </div>
+
+        </div>             
     </div>
   )
 }
